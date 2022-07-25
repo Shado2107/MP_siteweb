d82df3a2e5c7981583c89d30ee29e4222c0d6b9a
Shado2107 <pd39almeida@outlook.com>
2022-07-25 17:36:32 +0000

    ajoute de la section reasons debut

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
 import Hero from './components/Hero/Hero';
 import Services from './components/Services/Services';
+import Reasons from './components/Reasons/Reasons';
 
 function App() {
   return (
     <div className="App">
       <Hero/>
       <Services/>
+      <Reasons/>
     </div>
   );
 }
---
+++ src/components/Reasons/Reasons.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Reasons.css'
+
+const Reasons = () => {
+    return (
+        <div className="reasons" id="reasons">
+             <div className="left-r">
+                <img src="" alt="" />
+                <img src="" alt="" />
+                <img src="" alt="" />
+                <img src="" alt="" />
+             </div>
+             <div className="right-r"></div>
+        </div>
+    );
+};
+
+export default Reasons;
