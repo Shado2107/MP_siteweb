5e395bb549793c361ba9a4bc7b12c8d12f312784
Shado2107 <pd39almeida@outlook.com>
2022-08-02 10:01:52 +0000

    footer end

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,8 +1,24 @@
 import React from 'react';
+import './Footer.css'
+import instagram from '../../assets/instagram.png'
+import github from '../../assets/github.png'
+import logo from '../../assets/logo.png'
+import linkedin from '../../assets/linkedin.png'
 
 const Footer = () => {
     return (
-        <div>
+        <div className='footer-container'>
+            <hr/>
+            <div className="footer">
+                <div className="social-links">
+                    <img src={instagram} alt="" />
+                    <img src={github} alt="" />
+                    <img src={linkedin} alt="" />
+                </div> 
+                <div className="logo-f">
+                    <img src={logo} alt="" />
+                </div>   
+            </div>
             
         </div>
     );
